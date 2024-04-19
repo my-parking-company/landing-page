bd92bdd13c23bc0afcdc51d794eb4fffbdfa8122
Richard <richrd77@gmail.com>
2024-04-19 23:55:55 +0530

    added material

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { HomeComponent } from './components/home/home.component';
+import { MyMatModule } from '../my-mat/my-mat.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    MyMatModule,
   ]
 })
 export class MainModule { }
