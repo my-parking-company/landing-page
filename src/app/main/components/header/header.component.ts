137a5d508054b5861517f0682150e9629640fae9
Richard <richrd77@gmail.com>
2024-04-20 01:42:21 +0530

    responsvie menu

--- src/app/main/components/header/header.component.ts
+++ src/app/main/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  @ViewChild('menu', { read: ElementRef }) private menu!: ElementRef<HTMLUListElement>;
+  private isBurgerMenuShown = false;
+
+  public ShowMenu(event: any): void {
+    event.cancelBubble = true;
+    console.log(event, event.bubbles);
+    this.menu.nativeElement.classList.add('burger-show');
+    this.isBurgerMenuShown = true;
+  }
+
+  @HostListener('body:click')
+  public BodyClick(): void {
+    console.log('HHHH', this.isBurgerMenuShown);
+    if (this.isBurgerMenuShown) {
+      this.HideMenu();
+      this.isBurgerMenuShown = false;
+    }
+  }
+
+  private HideMenu(): void {
+    this.menu.nativeElement.classList.remove('burger-show');
+  }
 }
