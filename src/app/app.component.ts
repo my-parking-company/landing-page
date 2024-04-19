2412cf05c1b178424d45dc8d47a6c8a33be0d1de
Richard <richrd77@gmail.com>
2024-04-19 23:41:25 +0530

    another initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MyMatModule } from './mat/my-mat.module';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, MyMatModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
